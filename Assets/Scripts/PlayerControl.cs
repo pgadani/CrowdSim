e427ec1c89baad3932650d6eded0c67f4d0c6f3d
Pooja Gadani <pgadani123@gmail.com>
2016-08-20 22:44:40 -0400

    Animated

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour {
+
+	Animator animator;
+
+	void Start () {
+		animator = GetComponent<Animator>();	
+	}
+	
+	void Update () {
+		float v = Input.GetAxis("Vertical");
+		float h = Input.GetAxis("Horizontal");
+		if (Input.GetKeyDown(KeyCode.Space)) {
+			animator.SetTrigger("Jump");
+		}
+		else if (Input.GetKeyUp(KeyCode.Space)) {
+			animator.ResetTrigger("Jump");
+		}
+		if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift)) {
+			v*=2;
+		}
+		animator.SetFloat("Speed", v);
+		animator.SetFloat("Direction", h);
+		animator.SetFloat("Angle", 180*h);
+
+	}
+}
