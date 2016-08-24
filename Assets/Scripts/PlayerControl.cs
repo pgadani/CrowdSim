7dd3166a8b48dbea37ed31195b6d0dba2f5cdb02
Pooja Gadani <pgadani123@gmail.com>
2016-08-24 19:59:35 -0400

    Animations mostly work

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -4,12 +4,17 @@ using System.Collections;
 public class PlayerControl : MonoBehaviour {
 
 	Animator animator;
+	CapsuleCollider capsule;
+	public float capsuleHeight;
+	public float jumpHeight;
+	public float jumpDistance; // adjusted based on speed
 
-	void Start () {
-		animator = GetComponent<Animator>();	
+	void Start() {
+		animator = GetComponent<Animator>();
+		capsule = GetComponent<CapsuleCollider>();
 	}
 	
-	void Update () {
+	void Update() {
 		float v = Input.GetAxis("Vertical");
 		float h = Input.GetAxis("Horizontal");
 		if (Input.GetKeyDown(KeyCode.Space)) {
@@ -23,7 +28,19 @@ public class PlayerControl : MonoBehaviour {
 		}
 		animator.SetFloat("Speed", v);
 		animator.SetFloat("Direction", h);
-		animator.SetFloat("Angle", 180*h);
+	}
+
+	void FixedUpdate() {
+		AnimatorStateInfo currentBaseState = animator.GetCurrentAnimatorStateInfo(0);
+		if (currentBaseState.IsName("Base.LocomotionJump")) {
+			transform.Translate(Vector3.up*animator.GetFloat("JumpCurve")*jumpHeight);
+			capsule.height = capsuleHeight + animator.GetFloat("CapsuleCurve") * 2.0f;
+			transform.Translate(Vector3.forward*Time.deltaTime*jumpDistance*animator.GetFloat("Speed")*0.5f);
+		}
+		else if (currentBaseState.IsName("Base.IdleJump")) {
+			transform.Translate(Vector3.up*animator.GetFloat("JumpCurve")*jumpHeight);
+			capsule.height = capsuleHeight + animator.GetFloat("CapsuleCurve") * 0.7f;
+		}
 
 	}
 }
